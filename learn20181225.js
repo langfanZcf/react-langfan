37052e6b887e4240845f60bfc591eef8d739469d
langfanZcf <45536390+langfanZcf@users.noreply.github.com>
2019-01-02 20:52:45 +0800

    Add files via upload

---
+++ learn20181225.js
@@ -0,0 +1,87 @@
+// 定义一个数组数据
+import React from 'react';
+
+/**用来展示category标题*/
+class ProductCategoryRow extends React.Component{
+    render() {
+        return <tr> <th colSpan="2"> {this.props.category}</th></tr>
+    }
+}
+
+/**展示每个标题里展示的price，name*/
+class ProductRow extends React.Component{
+    render() {
+        var name =this.props.product.stocked ?
+            this.props.product.name : <span style={ {color : 'red'}}> {this.props.product.name}</span>
+        return (
+            <tr>
+                <td>{name}</td>
+                <td> {this.props.product.price}</td>
+            </tr>
+        );
+    }
+}
+
+/**画这个界面*/
+class ProductTable extends React.Component{
+    render (){
+        var rows =[];
+        var lastCategory =null;
+        this.props.products.forEach(function (product) {
+            if (product.category !== lastCategory) {
+                rows.push(<ProductCategoryRow category={product.category} key={product.category}/>);
+            }
+            rows.push(<ProductRow product={product} key={product.name}> </ProductRow>);
+            lastCategory = product.category;
+        });
+        return (
+            <table>
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>    Price</th>
+                </tr>
+                </thead>
+                <tbody>{rows}</tbody>
+            </table>
+        );
+    }
+}
+
+class SearchBar extends React.Component{
+
+    render() {
+        console.log("#####");
+        return(
+            <form>
+                <input type="text" placeholder="Search..." />
+                <p>
+                    <input type="checkbox" />
+                        {' '}
+                        Only show products in stock
+                </p>
+            </form>
+        );
+    }
+}
+
+class FilterableProductTable extends React.Component{
+    constructor(){
+        super();
+        this.state={ filterText: '', inStockOnly : false}
+    }
+    render(){
+        return(
+            <div>
+               <SearchBar />
+                <ProductTable  products={this.props.products} />
+            </div>
+        );
+    }
+}
+
+// ========================================
+export default FilterableProductTable
+
+
+
